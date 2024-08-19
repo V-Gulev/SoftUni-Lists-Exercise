5ea8ccb6a55f6d4884edb04ef0b85d6b16849fd7
V-Gulev <velizargulev@abv.bg>
2024-08-19 17:51:01 +0300

    Take/Skip Rope

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        String[] elements = input.split(" ");
-        int index = (int) Math.floor(elements.length/2);
-        int[] array1 = new int[index];
-        int[] array2 = new int[index];
-        int position = array2.length - 1;
-        for (int i = 0; i < index; i++) {
-            array1[i] = Integer.parseInt(elements[i]);
-        }
-        for (int i = elements.length - 1; i > index ; i--) {
-            array2[position] = Integer.parseInt(elements[i]);
-            position--;
-        }
+        StringBuilder result = new StringBuilder();
+        List<Integer> numberList = new ArrayList<>();
+        List<Character> nonNumberList = new ArrayList<>();
 
-        double car1 = 0;
-        for (int i = 0; i < index; i++) {
-            if (array1[i] == 0 && i != array1.length - 1) {
-                car1 = car1 * 0.8;
-            } else car1 += array1[i];
-
+        for (char j : input.toCharArray()) {
+            if (Character.isDigit(j)) {
+                numberList.add(Character.getNumericValue(j));
+            } else nonNumberList.add(j);
         }
-        double car2 = 0;
-        for (int i = array2.length - 1; i >= 0; i--) {
-            if (array2[i] == 0) {
-                car2 = car2 * 0.8;
-            } else car2 += array2[i];
-
+        List<Integer> takeList = new ArrayList<>();
+        List<Integer> skipList = new ArrayList<>();
+        for (int i = 0; i < numberList.size(); i++) {
+            if (i % 2 == 0) {
+                takeList.add(numberList.get(i));
+            } else skipList.add(numberList.get(i));
         }
 
-        if (car2 < car1) {
-            System.out.printf("The winner is right with total time: %.1f", car2);
-        } else System.out.printf("The winner is left with total time: %.1f", car1);
+        int index = 0;
+        for (int i = 0; i < takeList.size(); i++) {
+            int takeCount = takeList.get(i);
+            for (int j = 0; j < takeCount && index < nonNumberList.size(); j++) {
+                result.append(nonNumberList.get(index));
+                index++;
+            }
+            index += skipList.get(i);
+        }
+        System.out.println(result.toString());
 
     }
 }
