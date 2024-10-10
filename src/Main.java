47b7c1e6324279c7aa3123657cef2132148f52ee
V-Gulev <velizargulev@abv.bg>
2024-10-10 16:04:47 +0300

    List Manipulation Basics

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,47 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double budget = Double.parseDouble(scanner.nextLine());
-        String input = scanner.nextLine();
-        String[] elements = input.split(" ");
-        List<Integer> drumQuality = new ArrayList<>();
-        for (String element : elements) {
-            drumQuality.add(Integer.parseInt(element));
-        }
-        List<Integer> startingDrumQuality = new ArrayList<>(drumQuality);
 
-        while (true) {
-            String command = scanner.nextLine();
-            if (command.equals("Hit it again, Gabsy!")) {
-                break;
-            }
-            int damage = Integer.parseInt(command);
-            int index = 0;
-            for (int i = 0; i < drumQuality.size(); i++) {
-                drumQuality.set(index, drumQuality.get(i) - damage);
-                if (drumQuality.get(index) <= 0 && budget >= startingDrumQuality.get(index) * 3) {
-                    drumQuality.set(index,startingDrumQuality.get(index));
-                    budget -= startingDrumQuality.get(index) * 3;
-                    index++;
-                } else if (drumQuality.get(index) <=0) {
-                    drumQuality.remove(index);
-                    startingDrumQuality.remove(index);
-                    i--;
-                } else index++;
+        List<Integer> nums = new ArrayList<>(Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toList());
+        String[] input = scanner.nextLine().split(" ");
+        String command = input[0];
+        int number1;
+        int number2 = 0;
+        while (!command.equals("end")) {
 
+            number1 = Integer.parseInt(input[1]);
+            if (command.equals("Insert")) {
+                number2 = Integer.parseInt(input[2]);
             }
 
+            switch (command) {
+                case "Add":
+                    nums.add(number1);
+                    break;
+                case "Remove":
+                    nums.remove(Integer.valueOf(number1));
+                    break;
+                case "RemoveAt":
+                    nums.remove(number1);
+                    break;
+                case "Insert":
+                    nums.add(number2,number1);
+                    break;
+                default:
+                    break;
+            }
 
+            input = scanner.nextLine().split(" ");
+            command = input[0];
         }
-        for (int i = 0; i < drumQuality.size(); i++) {
-            System.out.print(drumQuality.get(i) + " ");
+        for (int num : nums) {
+            System.out.print(num + " ");
         }
-        System.out.println();
-        System.out.printf("Gabsy has %.2flv.",budget);
     }
 }
