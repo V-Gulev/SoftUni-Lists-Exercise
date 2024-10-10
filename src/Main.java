54dd63424a32acb69b5ebe06129695ee104993e0
V-Gulev <velizargulev@abv.bg>
2024-10-10 16:23:45 +0300

    Gauss' Trick

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        List<Integer> nums = new ArrayList<>(Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toList());
-        String[] input = scanner.nextLine().split(" ");
-        String command = input[0];
-        int number1;
-        int number2 = 0;
-        while (!command.equals("end")) {
-
-            number1 = Integer.parseInt(input[1]);
-            if (command.equals("Insert")) {
-                number2 = Integer.parseInt(input[2]);
-            }
-
-            switch (command) {
-                case "Add":
-                    nums.add(number1);
-                    break;
-                case "Remove":
-                    nums.remove(Integer.valueOf(number1));
-                    break;
-                case "RemoveAt":
-                    nums.remove(number1);
-                    break;
-                case "Insert":
-                    nums.add(number2,number1);
-                    break;
-                default:
-                    break;
-            }
-
-            input = scanner.nextLine().split(" ");
-            command = input[0];
+        List<Integer> input = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> result = new ArrayList<>();
+        while (input.size() > 1) {
+            int a = input.get(0) + input.get(input.size()-1);
+            input.remove(0);
+            input.remove(input.size() - 1);
+            result.add(a);
         }
-        for (int num : nums) {
-            System.out.print(num + " ");
+        if (!input.isEmpty()){
+            result.add(input.get(0));
         }
+    for (int num : result){
+        System.out.print(num + " ");
+    }
+
     }
 }
