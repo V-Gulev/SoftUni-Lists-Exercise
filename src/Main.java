015c324d5a2234ba0751f9ecdd3da4041a91b838
V-Gulev <velizargulev@abv.bg>
2024-10-11 14:42:50 +0300

    Mixed up Lists

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,57 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        StringBuilder result = new StringBuilder();
-        List<Integer> numberList = new ArrayList<>();
-        List<Character> nonNumberList = new ArrayList<>();
+        String input1 = scanner.nextLine();
+        String input2 = scanner.nextLine();
+        String[] elements1 = input1.split(" ");
+        String[] elements2 = input2.split(" ");
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
 
-        for (char j : input.toCharArray()) {
-            if (Character.isDigit(j)) {
-                numberList.add(Character.getNumericValue(j));
-            } else nonNumberList.add(j);
-        }
-        List<Integer> takeList = new ArrayList<>();
-        List<Integer> skipList = new ArrayList<>();
-        for (int i = 0; i < numberList.size(); i++) {
-            if (i % 2 == 0) {
-                takeList.add(numberList.get(i));
-            } else skipList.add(numberList.get(i));
+        for (String j : elements1) {
+            list1.add(Integer.parseInt(j));
         }
 
-        int index = 0;
-        for (int i = 0; i < takeList.size(); i++) {
-            int takeCount = takeList.get(i);
-            for (int j = 0; j < takeCount && index < nonNumberList.size(); j++) {
-                result.append(nonNumberList.get(index));
-                index++;
+        for (String k : elements2) {
+            list2.add(Integer.parseInt(k));
+        }
+
+        int end = Math.min(list1.size(), list2.size());
+        int index = list2.size() - 1;
+        List<Integer> mixedList = new ArrayList<>();
+
+        for (int i = 0; i < end; i++) {
+            mixedList.add(list1.get(i));
+            mixedList.add(list2.get(index));
+            index--;
+        }
+        int startRange;
+        int endRange;
+        if (list1.size() > list2.size()) {
+            endRange = list1.getLast();
+            list1.removeLast();
+            startRange = list1.getLast();
+        } else {
+            endRange = list2.getFirst();
+            list2.removeFirst();
+            startRange = list2.getFirst();
+        }
+
+        if (startRange > endRange) {
+            int temp = startRange;
+            startRange = endRange;
+            endRange = temp;
+        }
+        mixedList.sort(Comparator.naturalOrder());
+        for (Integer integer : mixedList) {
+            if (integer > startRange && integer < endRange) {
+                System.out.print(integer + " ");
             }
-            index += skipList.get(i);
         }
-        System.out.println(result);
-
     }
 }
