609743ef9fcf6d3bd602154ecf470aef40cddf62
V-Gulev <velizargulev@abv.bg>
2024-10-11 14:37:18 +0300

    Remove Negatives and Reverse

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = Integer.parseInt(sc.nextLine());
-        List<String> items = new ArrayList<>();
-        for (int i = 0; i < num; i++) {
-            String input = sc.nextLine();
-            items.add(input);
+        List<Integer> numbers = new ArrayList<>(Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toList());
+        numbers.removeIf(integer -> integer < 0);
+        if (numbers.isEmpty()) {
+            System.out.println("empty");
+        } else {
+            Collections.reverse(numbers);
+            System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
         }
-        items.sort(Comparator.naturalOrder());
-        for (int i = 1; i <= items.size(); i++) {
-            System.out.printf("%d.%s%n", i, items.get(i - 1));
-        }
+
     }
 }
