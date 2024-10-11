15fa4e347c7ddeb8a26804f84f55da84b7d5b322
V-Gulev <velizargulev@abv.bg>
2024-10-11 14:35:13 +0300

    Merging Lists

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> input = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list1 = new ArrayList<>(Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toList());
+        List<Integer> list2 = new ArrayList<>(Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toList());
         List<Integer> result = new ArrayList<>();
-        while (input.size() > 1) {
-            int a = input.get(0) + input.get(input.size() - 1);
-            input.remove(0);
-            input.remove(input.size() - 1);
-            result.add(a);
+
+        while (!list2.isEmpty() && !list1.isEmpty()) {
+            result.add(list1.getFirst());
+            result.add(list2.getFirst());
+            list1.removeFirst();
+            list2.removeFirst();
         }
-        if (!input.isEmpty()) {
-            result.add(input.get(0));
+
+        if (!list1.isEmpty()) {
+            result.addAll(list1);
+        } else if (!list2.isEmpty()) {
+            result.addAll(list2);
         }
         for (int num : result) {
             System.out.print(num + " ");
         }
-
     }
 }
