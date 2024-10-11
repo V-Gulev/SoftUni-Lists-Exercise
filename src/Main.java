f2b92a90e3d442af2be373dffc9dfb336801c60d
V-Gulev <velizargulev@abv.bg>
2024-10-11 14:57:50 +0300

    Train

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,31 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double budget = Double.parseDouble(scanner.nextLine());
-        String input = scanner.nextLine();
-        String[] elements = input.split(" ");
-        List<Integer> drumQuality = new ArrayList<>();
-        for (String element : elements) {
-            drumQuality.add(Integer.parseInt(element));
-        }
-        List<Integer> startingDrumQuality = new ArrayList<>(drumQuality);
-
+        List<Integer> trainWagons = new ArrayList<>(Arrays.stream((scanner.nextLine().split(" "))).map(Integer::parseInt).toList());
+        int maxCapacity = Integer.parseInt(scanner.nextLine());
         while (true) {
-            String command = scanner.nextLine();
-            if (command.equals("Hit it again, Gabsy!")) {
+            String[] command = scanner.nextLine().split(" ");
+            if (command[0].equals("end")) {
                 break;
-            }
-            int damage = Integer.parseInt(command);
-            int index = 0;
-            for (int i = 0; i < drumQuality.size(); i++) {
-                drumQuality.set(index, drumQuality.get(i) - damage);
-                if (drumQuality.get(index) <= 0 && budget >= startingDrumQuality.get(index) * 3) {
-                    drumQuality.set(index, startingDrumQuality.get(index));
-                    budget -= startingDrumQuality.get(index) * 3;
-                    index++;
-                } else if (drumQuality.get(index) <= 0) {
-                    drumQuality.remove(index);
-                    startingDrumQuality.remove(index);
-                    i--;
-                } else index++;
-
+            } else if (command[0].equals("Add")) {
+                trainWagons.add(Integer.parseInt(command[1]));
+            } else {
+                for (int i = 0; i < trainWagons.size(); i++) {
+                    if (trainWagons.get(i) + Integer.parseInt(command[0]) <= maxCapacity) {
+                        trainWagons.set(i, trainWagons.get(i) + Integer.parseInt(command[0]));
+                        break;
+                    }
+                }
             }
 
-
         }
-        for (Integer integer : drumQuality) {
-            System.out.print(integer + " ");
+        for (int wagon : trainWagons) {
+            System.out.print(wagon + " ");
         }
-        System.out.println();
-        System.out.printf("Gabsy has %.2flv.", budget);
     }
 }
