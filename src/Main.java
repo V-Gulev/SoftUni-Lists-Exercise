07db6756bbd7f91a28a2a42f6a89fe1cf6a70105
V-Gulev <velizargulev@abv.bg>
2024-10-12 18:51:45 +0300

    Bomb Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,32 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> numbers = new ArrayList<>(Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toList());
+        List<Integer> numbers = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        int specialNumber = sc.nextInt();
+        int powerOfNumber = sc.nextInt();
+        int index;
+        int sum = 0;
+        while (numbers.contains(specialNumber)) {
 
-        while (true) {
-            String[] input = sc.nextLine().split(" ");
-            String command = input[0];
-            if (input[0].equals("End")) {
-                break;
+            for (int i = 1; i <= powerOfNumber; i++) {
+                index = numbers.indexOf(specialNumber);
+                if (index < numbers.size() - 1) {
+                    numbers.remove(index + 1);
+                }
+                index = numbers.indexOf(specialNumber);
+                if (index != 0) {
+                    numbers.remove(index - 1);
+                }
             }
-
-            switch (command) {
-                case "Add":
-                    numbers.add(Integer.parseInt(input[1]));
-                    break;
-                case "Remove":
-                    int removeIndex = Integer.parseInt(input[1]);
-                    if (removeIndex >= numbers.size() || removeIndex < 0) {
-                        System.out.println("Invalid index");
-                    } else {
-                        numbers.remove(removeIndex);
-                    }
-                    break;
-                case "Insert":
-                    int insertIndex = Integer.parseInt(input[2]);
-                    if (insertIndex > numbers.size() || insertIndex < 0) {
-                        System.out.println("Invalid index");
-                    } else {
-                        numbers.add(insertIndex, Integer.parseInt(input[1]));
-                    }
-                    break;
-                case "Shift":
-                    if (input[1].equals("left")) {
-                        shiftLeft(numbers, Integer.parseInt(input[2]));
-                    } else {
-                        shiftRight(numbers, Integer.parseInt(input[2]));
-                    }
-                    break;
-                default:
-                    break;
-            }
+            numbers.remove((Integer) specialNumber);
         }
         for (int num : numbers) {
-            System.out.print(num + " ");
+            sum += num;
         }
-    }
 
-    public static void shiftLeft(List<Integer> numbers, int timesRepeat) {
-        for (int i = 0; i < timesRepeat; i++) {
-            numbers.add(numbers.get(0));
-            numbers.remove(0);
-        }
-    }
-
-    public static void shiftRight(List<Integer> numbers, int timesRepeat) {
-        for (int i = 0; i < timesRepeat; i++) {
-            numbers.add(0, numbers.get(numbers.size() - 1));
-            numbers.remove(numbers.size() - 1);
-        }
+        System.out.println(sum);
     }
 }
