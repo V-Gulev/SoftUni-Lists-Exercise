7078b1d6fc4bdbfafcae642af7dbbf50f6c3123f
V-Gulev <velizargulev@abv.bg>
2024-10-13 17:21:47 +0300

    SoftUni Course Planning

--- src/Main.java
+++ src/Main.java
@@ -7,52 +7,74 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> pokemons = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int removedNumbersSum = 0;
-        while (!pokemons.isEmpty()) {
-            int index = Integer.parseInt(sc.nextLine());
+        List<String> schedule = Arrays.stream(sc.nextLine().split(", ")).collect(Collectors.toList());
+        String input = sc.nextLine();
 
-            if (index < 0) {
-                int setNumber = pokemons.getLast();
-                int removedNumber = pokemons.getFirst();
-                removedNumbersSum += removedNumber;
-                pokemons.removeFirst();
-                pokemons.addFirst(setNumber);
-                for (int i = 0; i < pokemons.size(); i++) {
-                    if (pokemons.get(i) <= removedNumber) {
-                        pokemons.set(i, pokemons.get(i) + removedNumber);
-                    } else {
-                        pokemons.set(i, pokemons.get(i) - removedNumber);
-                    }
-                }
-            } else if (index >= pokemons.size()) {
-                int setNumber = pokemons.getFirst();
-                int removedNumber = pokemons.getLast();
-                removedNumbersSum += removedNumber;
-                pokemons.removeLast();
-                pokemons.add(setNumber);
+        while (!input.equals("course start")) {
+            String[] commands = input.split(":");
 
-                for (int i = 0; i < pokemons.size(); i++) {
-                    if (pokemons.get(i) <= removedNumber) {
-                        pokemons.set(i, pokemons.get(i) + removedNumber);
-                    } else {
-                        pokemons.set(i, pokemons.get(i) - removedNumber);
+            switch (commands[0]) {
+                case "Add":
+                    if (!schedule.contains(commands[1])) {
+                        schedule.add(commands[1]);
                     }
-                }
-            } else {
-                int removedNumber = pokemons.remove(index);
-                removedNumbersSum += removedNumber;
-                for (int i = 0; i < pokemons.size(); i++) {
-                    if (pokemons.get(i) <= removedNumber) {
-                        pokemons.set(i, pokemons.get(i) + removedNumber);
-                    } else {
-                        pokemons.set(i, pokemons.get(i) - removedNumber);
+                    break;
+                case "Insert":
+                    if (!schedule.contains(commands[1])) {
+                        schedule.add(Integer.parseInt(commands[2]), commands[1]);
                     }
-                }
+                    break;
+                case "Remove":
+                    if (schedule.contains(commands[1])) {
+                        int index = schedule.indexOf(commands[1]);
+                        schedule.remove(index);
+
+                        if (index < schedule.size() && schedule.get(index).equals(commands[1] + "-Exercise")) {
+                            schedule.remove(index);
+                        }
+                    }
+                    break;
+                case "Swap":
+                    if (schedule.contains(commands[1]) && schedule.contains(commands[2])) {
+                        int index1 = schedule.indexOf(commands[1]);
+                        int index2 = schedule.indexOf(commands[2]);
+
+
+                        String temp = schedule.get(index1);
+                        schedule.set(index1, schedule.get(index2));
+                        schedule.set(index2, temp);
+
+
+                        if (index1 + 1 < schedule.size() && schedule.get(index1 + 1).equals(commands[1] + "-Exercise")) {
+                            String exercise = schedule.remove(index1 + 1);
+                            schedule.add(index2 + 1, exercise);
+                        }
+
+                        if (index2 + 1 < schedule.size() && schedule.get(index2 + 1).equals(commands[2] + "-Exercise")) {
+                            String exercise = schedule.remove(index2 + 1);
+                            schedule.add(index1 + 1, exercise);
+                        }
+                    }
+                    break;
+                case "Exercise":
+                    if (schedule.contains(commands[1])) {
+                        int index = schedule.indexOf(commands[1]);
+                        if (index + 1 >= schedule.size() || !schedule.get(index + 1).equals(commands[1] + "-Exercise")) {
+                            schedule.add(index + 1, commands[1] + "-Exercise");
+                        }
+                    } else {
+                        schedule.add(commands[1]);
+                        schedule.add(commands[1] + "-Exercise");
+                    }
+                    break;
+                default:
+                    break;
             }
-
+            input = sc.nextLine();
         }
-        System.out.println(removedNumbersSum);
 
+        for (int i = 0; i < schedule.size(); i++) {
+            System.out.printf("%d.%s%n", i + 1, schedule.get(i));
+        }
     }
 }
