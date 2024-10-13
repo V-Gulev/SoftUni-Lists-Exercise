6c0d0723a9f0d25650cae406303c43b3173ddb70
V-Gulev <velizargulev@abv.bg>
2024-10-13 16:09:41 +0300

    Pokemon Don't Go

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,58 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> data = new ArrayList<>(List.of(sc.nextLine().split("\\s+")));
-        String command = sc.nextLine();
-        while (!command.equals("3:1")) {
-            String[] input = command.split("\\s+");
-            String action = input[0];
+        List<Integer> pokemons = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        int removedNumbersSum = 0;
+        while (!pokemons.isEmpty()) {
+            int index = Integer.parseInt(sc.nextLine());
 
-            switch (action) {
-                case "merge":
-                    int startIndex = Math.max(Integer.parseInt(input[1]), 0);
-                    int endIndex = Math.min(Integer.parseInt(input[2]), data.size() - 1);
-                    if (startIndex <= endIndex) {
-                        merge(data, startIndex, endIndex);
+            if (index < 0) {
+                int setNumber = pokemons.getLast();
+                int removedNumber = pokemons.getFirst();
+                removedNumbersSum += removedNumber;
+                pokemons.removeFirst();
+                pokemons.addFirst(setNumber);
+                for (int i = 0; i < pokemons.size(); i++) {
+                    if (pokemons.get(i) <= removedNumber) {
+                        pokemons.set(i, pokemons.get(i) + removedNumber);
+                    } else {
+                        pokemons.set(i, pokemons.get(i) - removedNumber);
                     }
-                    break;
+                }
+            } else if (index >= pokemons.size()) {
+                int setNumber = pokemons.getFirst();
+                int removedNumber = pokemons.getLast();
+                removedNumbersSum += removedNumber;
+                pokemons.removeLast();
+                pokemons.add(setNumber);
 
-                case "divide":
-                    int index = Integer.parseInt(input[1]);
-                    int parts = Integer.parseInt(input[2]);
-                    if (index >= 0 && index < data.size() && parts > 0) {
-                        divide(data, index, parts);
+                for (int i = 0; i < pokemons.size(); i++) {
+                    if (pokemons.get(i) <= removedNumber) {
+                        pokemons.set(i, pokemons.get(i) + removedNumber);
+                    } else {
+                        pokemons.set(i, pokemons.get(i) - removedNumber);
                     }
-                    break;
-                default:
-                    break;
-            }
-
-            command = sc.nextLine();
-        }
-
-        System.out.println(String.join(" ", data));
-    }
-
-    public static void merge(List<String> data, int startIndex, int endIndex) {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = startIndex; i <= endIndex; i++) {
-            result.append(data.get(i));
-        }
-
-        for (int i = endIndex; i >= startIndex; i--) {
-            data.remove(i);
-        }
-
-        data.add(startIndex, result.toString());
-    }
-
-    public static void divide(List<String> data, int index, int parts) {
-        String word = data.get(index);
-        int partSize = word.length() / parts;
-
-
-        List<String> dividedParts = new ArrayList<>();
-
-        int currentIndex = 0;
-        for (int i = 0; i < parts; i++) {
-            if (i == parts - 1) {
-                dividedParts.add(word.substring(currentIndex));
+                }
             } else {
-                dividedParts.add(word.substring(currentIndex, currentIndex + partSize));
-                currentIndex += partSize;
+                int removedNumber = pokemons.remove(index);
+                removedNumbersSum += removedNumber;
+                for (int i = 0; i < pokemons.size(); i++) {
+                    if (pokemons.get(i) <= removedNumber) {
+                        pokemons.set(i, pokemons.get(i) + removedNumber);
+                    } else {
+                        pokemons.set(i, pokemons.get(i) - removedNumber);
+                    }
+                }
             }
-        }
 
-        data.remove(index);
-        data.addAll(index, dividedParts);
+        }
+        System.out.println(removedNumbersSum);
+
     }
 }
